965385f41897d87b23ce0e8093ccae82fc39560c
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2023-11-19 18:28:02 +0900

    Change social links, Remove contributors screen

--- lib/ui/widgets/settingsView/social_media_widget.dart
+++ lib/ui/widgets/settingsView/social_media_widget.dart
@@ -4,7 +4,6 @@ import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:revanced_manager/ui/widgets/settingsView/social_media_item.dart';
 import 'package:revanced_manager/ui/widgets/shared/custom_card.dart';
-import 'package:revanced_manager/ui/widgets/shared/custom_icon.dart';
 
 class SocialMediaWidget extends StatelessWidget {
   const SocialMediaWidget({
@@ -44,46 +43,22 @@ class SocialMediaWidget extends StatelessWidget {
           child: Column(
             children: <Widget>[
               SocialMediaItem(
-                icon: Icon(CustomIcon.revancedIcon),
-                title: Text('Website'),
-                subtitle: Text('revanced.app'),
-                url: 'https://revanced.app',
-              ),
-              SocialMediaItem(
                 icon: FaIcon(FontAwesomeIcons.github),
                 title: Text('GitHub'),
-                subtitle: Text('github.com/ReVanced'),
-                url: 'https://github.com/ReVanced',
-              ),
-              SocialMediaItem(
-                icon: FaIcon(FontAwesomeIcons.discord),
-                title: Text('Discord'),
-                subtitle: Text('discord.gg/revanced'),
-                url: 'https://discord.gg/rF2YcEjcrT',
+                subtitle: Text('kitadai31/revanced-manager-android5-7'),
+                url: 'https://github.com/kitadai31/revanced-manager-android5-7',
               ),
               SocialMediaItem(
                 icon: FaIcon(FontAwesomeIcons.telegram),
                 title: Text('Telegram'),
-                subtitle: Text('t.me/app_revanced'),
-                url: 'https://t.me/app_revanced',
+                subtitle: Text('t.me/rvx_for_a6_7'),
+                url: 'https://t.me/rvx_for_a6_7',
               ),
               SocialMediaItem(
-                icon: FaIcon(FontAwesomeIcons.reddit),
-                title: Text('Reddit'),
-                subtitle: Text('r/revancedapp'),
-                url: 'https://reddit.com/r/revancedapp',
-              ),
-              SocialMediaItem(
-                icon: FaIcon(FontAwesomeIcons.xTwitter),
-                title: Text('X'),
-                subtitle: Text('@revancedapp'),
-                url: 'https://x.com/revancedapp',
-              ),
-              SocialMediaItem(
-                icon: FaIcon(FontAwesomeIcons.youtube),
-                title: Text('YouTube'),
-                subtitle: Text('youtube.com/@revanced'),
-                url: 'https://youtube.com/@revanced',
+                icon: FaIcon(FontAwesomeIcons.telegram),
+                title: Text('Telegram chat group'),
+                subtitle: Text('t.me/rvx_for_a6_7_chat'),
+                url: 'https://t.me/rvx_for_a6_7_chat',
               ),
             ],
           ),
