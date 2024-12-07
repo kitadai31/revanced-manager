e05635d7e3944b7ad2bf926495ca03578f86a201
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-12-07 14:47:13 +0900

    feat: show all network errors in Toast
    (cherry picked from commit f164f235921b0b83660871a6825d4b32ed528681)

--- lib/services/download_manager.dart
+++ lib/services/download_manager.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
 import 'package:flutter/foundation.dart';
@@ -6,6 +7,7 @@ import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:injectable/injectable.dart';
 import 'package:revanced_manager/app/app.locator.dart';
 import 'package:revanced_manager/services/manager_api.dart';
+import 'package:revanced_manager/services/toast.dart';
 
 @lazySingleton
 class DownloadManager {
@@ -41,6 +43,7 @@ class DownloadManager {
     }
 
     dio.interceptors.add(DioCacheInterceptor(options: _cacheOptions));
+    dio.interceptors.add(_ShowToastInterceptor());
     return dio;
   }
 
@@ -71,5 +74,45 @@ class DownloadManager {
         'User-Agent': _userAgent,
       },
     );
+  }
+}
+
+class _ShowToastInterceptor extends Interceptor {
+  final Toast _toast = locator<Toast>();
+  bool _noConnection = false;
+  _ShowToastInterceptor() {
+    final Future connectivityResult = Connectivity().checkConnectivity();
+    connectivityResult.then((result) {
+      _noConnection = result.contains(ConnectivityResult.none);
+    });
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    final response = err.response;
+    if (response != null) {
+      // Check if the error is rate limit
+      if (response.headers['x-ratelimit-remaining']?[0] == '0') {
+        final resetUnixTime =
+            int.parse(response.headers['x-ratelimit-reset']?[0] ?? '0');
+        final resetDateTime =
+            DateTime.fromMillisecondsSinceEpoch(resetUnixTime * 1000);
+        final remainingMinutes =
+            resetDateTime.difference(DateTime.now()).inMinutes;
+        _toast.showBottom(
+            'GitHub API rate limit exceeded. Change the network or wait $remainingMinutes minutes.');
+      } else {
+        _toast.showBottom(
+            '${response.statusCode} ${response.statusMessage}: ${err.requestOptions.uri}');
+      }
+    } else {
+      // Show errors only when connected to network
+      if (!_noConnection) {
+        // The "DioException" text is unnecessary for users, so remove it to simplify
+        _toast.showBottom(err.requestOptions.uri.host +
+            err.toString().replaceFirst('DioException', ''));
+      }
+    }
+    super.onError(err, handler);
   }
 }
